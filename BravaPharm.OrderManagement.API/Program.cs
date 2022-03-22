b181c7ff04d35310ae030ed780aea2af91af0628
Mustafa Bulgu <mustafabulgu@gmail.com>
2022-03-22 18:50:37 +0000

    removed app project

--- BravaPharm.OrderManagement.API/Program.cs
+++ BravaPharm.OrderManagement.API/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+app.UseDeveloperExceptionPage();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 {
