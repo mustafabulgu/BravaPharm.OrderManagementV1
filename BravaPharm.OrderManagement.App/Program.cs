b4c474e6a313ab82675e9486bc93c96edb6ae3dd
Mustafa Bulgu <mustafabulgu@gmail.com>
2022-04-13 05:00:51 +0100

    Read api url from configuration.

--- BravaPharm.OrderManagement.App/Program.cs
+++ BravaPharm.OrderManagement.App/Program.cs
@@ -9,11 +9,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var apiUrl = builder.Configuration["ApiUrl"];
 
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 //builder.Services.AddHttpClient<IClient, Client>(client=>client.BaseAddress = new Uri("https://localhost:7077"));
-builder.Services.AddHttpClient<IClient, Client>(_ => new Client("https://localhost:7077", new HttpClient { BaseAddress = new Uri("https://localhost:7077") }) );
+builder.Services.AddHttpClient<IClient, Client>(_ => new Client(apiUrl, new HttpClient { BaseAddress = new Uri(apiUrl) }) );
 builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
 await builder.Build().RunAsync();
